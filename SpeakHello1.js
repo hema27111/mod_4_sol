1f85d560623fb34ff8dbceb2e4454d0e8f945eb4
Hema Aggarwal <15mec001@lnmiit.ac.in>
2018-05-09 16:59:57 -0600

    Error

--- SpeakHello1.js
+++ SpeakHello1.js
@@ -3,7 +3,7 @@
 	//helloSpeaker.name =[];
 	var speakWord = "Hello";
 	helloSpeaker.speak = function(names) {
-  		console.log(speakWord + " " +names);
+  		console.log(speakWord +names);
 	}
 	window.helloSpeaker =helloSpeaker;
 })(window);
